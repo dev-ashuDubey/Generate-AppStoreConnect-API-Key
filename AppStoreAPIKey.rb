bc12b42089785bc0b8576a903b61acc0a0a1e7ad
Ashutosh Dubey <39190710+dev-ashuDubey@users.noreply.github.com>
2023-08-04 14:14:17 +0200

    Update AppStoreAPIKey.rb

--- AppStoreAPIKey.rb
+++ AppStoreAPIKey.rb
@@ -1,29 +1,27 @@
 require 'jwt'
 require 'openssl'
 
-lane :get_api_token do |options|
-    key_id = "-----"
-    issuer_id = "-----"
-    key = "-----BEGIN PRIVATE KEY----- KEY KEY KEY -----END PRIVATE KEY-----"
+key_id = "-----"
+issuer_id = "-----"
+key = "-----BEGIN PRIVATE KEY----- KEY KEY KEY -----END PRIVATE KEY-----"
 
-    peky = OpenSSL::PKey::EC.new(key)
+peky = OpenSSL::PKey::EC.new(key)
     
-    now = Time.now
-    expiration = now + 1200
+now = Time.now
+expiration = now + 1200
 
-    header = {
-      kid: key_id,
-      typ: 'JWT'
-    }
+header = {
+    kid: key_id,
+    typ: 'JWT'
+}
 
-    payload = {
-      iss: issuer_id,
-      iat: now.to_i,
-      exp: expiration.to_i,
-      aud: 'appstoreconnect-v1'
-    }
+payload = {
+    iss: issuer_id,
+    iat: now.to_i,
+    exp: expiration.to_i,
+    aud: 'appstoreconnect-v1'
+}
 
-    api_token = JWT.encode(payload, peky, 'ES256', header)
+api_token = JWT.encode(payload, peky, 'ES256', header)
 
-    puts api_token
-end
+puts api_token
